2ed57dc3f2c8faf0d439ecd638f0291bb82ecc0d
dominicz <dominicz@medadvisor.com.au>
2022-08-10 20:24:22 +1000

    Add StripComment

---
+++ CodingKata/CodingKata/StripComment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingKata
+{
+    public class StripComment
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            return string.Join(
+                "\n", 
+                text.Split("\n")
+                .Select(x => (x.Any(c => commentSymbols.Contains(c.ToString())) ? x.Substring(0, x.IndexOf(x.First(x => commentSymbols.Contains(x.ToString())))) : x).TrimEnd())
+            );
+        }
+    }
+}
---
+++ CodingKata/TestProject/StripCommentTest.cs
@@ -0,0 +1,25 @@
+﻿using CodingKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class StripCommentTest
+    {
+        [TestMethod]
+        public void StripComments()
+        {
+            Assert.AreEqual(
+                    "apples, pears\ngrapes\nbananas",
+                    StripComment.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }));
+
+            Assert.AreEqual("a\nc\nd", StripComment.StripComments("a #b\nc\nd $e f g", new string[] { "#", "$" }));
+
+        }
+    }
+}
